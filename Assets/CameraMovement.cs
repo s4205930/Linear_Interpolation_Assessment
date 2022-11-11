90c120a90539af1a76d43dc3363bc1e131d7b378
s4205930 <114227654+s4205930@users.noreply.github.com>
2022-11-11 16:13:10 +0000

    mouse

--- Assets/CameraMovement.cs
+++ Assets/CameraMovement.cs
@@ -5,11 +5,15 @@ using UnityEngine;
 public class CameraMovement : MonoBehaviour
 {
     private float speed = 10f;
+    private float mouseSens = 1f;
     void Update()
     {
 
         Vector3 move = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0f);
         transform.position += move * speed * Time.deltaTime;
+
+        Vector2 rotate = new Vector2(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y")) * mouseSens * Time.deltaTime;
+        transform.Rotate(rotate.x, rotate.y);
         
     }
 }
